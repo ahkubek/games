70f9c05cd94d1517613630d905b88200b43aaa92
ahkubek <120653318+ahkubek@users.noreply.github.com>
2024-01-10 16:36:47 +0300

    угадай числоV.2

--- src/main/java/com/evil/Main.java
+++ src/main/java/com/evil/Main.java
@@ -7,22 +7,25 @@ import  java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         int a;
-        Scanner sc = new Scanner(System.in);
 
-        System.out.println("игра угадай загаданное число ");
-      int  num= (int)(Math.random()*100);
-        System.out.println("загаданное число"+num);
+            Scanner sc = new Scanner(System.in);
 
-        a = sc.nextInt();
+            System.out.println("игра угадай загаданное число ");
+            int num = (int) (Math.random() * 100);
+            System.out.println("загаданное число" + num);
+while (true){
+            a = sc.nextInt();
 
-       if (num==a) System.out.println("это праавильный ответ");
-       else if (num<a) {
-           System.out.println("Бери меньше");
+            if (num < a) System.out.println("Бери меньше");
 
-       }else  {
-           System.out.println("бери больше");
-       }
+            else if (num > a) {
+                System.out.println("Бери оьльше ");
 
+            } else if (a==num){
+                System.out.println("*** ПОБЕДА *****");
+                break;
+            }
+        }
 
     }
 }
